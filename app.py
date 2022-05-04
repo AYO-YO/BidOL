b7cc903f569887f06916365fa4fe94fba001213e
ayo_yo <gxlandzcx@gmail.com>
2022-05-04 15:04:37 +0800

    Initial Commit

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return '刘晨旭是个老哈皮'
+def index():  # put application's code here
+    return render_template('login.html')
+
+
+@app.route('/footer', methods=['GET', 'POST'])
+def footer():
+    return render_template('footer.html')
+
+
+@app.route('/header', methods=['GET', 'POST'])
+def header():
+    return render_template('header.html')
 
 
 @app.route('/main', methods=['GET', 'POST'])
